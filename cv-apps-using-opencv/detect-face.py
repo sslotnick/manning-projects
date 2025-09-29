7e709df3af3a4b36a145ae167f6781446b6beb35
Steve Slotnick <sslotnick@gmail.com>
2025-09-29 13:25:04 -0700

    part 2

--- cv-apps-using-opencv/detect-face.py
+++ cv-apps-using-opencv/detect-face.py
@@ -2,32 +2,42 @@
 
 import cv2;
 
-def captureImage():
-    cap = cv2.VideoCapture(0)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    writer = cv2.VideoWriter(
-        "face-detect-output.mp4",
-        cv2.VideoWriter_fourcc(*'mp4v'), fps,
-        (width, height))
 
-    frame = None
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        cv2.imshow("Video", frame)
-        key = cv2.waitKey(1)
-        if key == ord('c') or key == 27:
-            break
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = cap.get(cv2.CAP_PROP_FPS)
+writer = cv2.VideoWriter(
+    "face-detect-output.mp4",
+    cv2.VideoWriter_fourcc(*'mp4v'), fps,
+    (width, height))
 
-        writer.write(frame)
+classifier = cv2.CascadeClassifier()
+classifier.load("haar-classifier.xml")
 
-    cap.release()
-    cv2.destroyWindow("Video")
-    writer.release()
-    return frame
+frame = None
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-if __name__ == "__main__":
-    captureImage()
+    faces = classifier.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=3, minSize=(100, 100))
+
+    num_faces = len(faces)
+    for i in range(num_faces):
+        x, y, w, h = faces[i]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255 - i * 100), 2)
+
+    cv2.imshow("Video", frame)
+    writer.write(frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('c') or key == 27:
+        break
+
+
+
+cap.release()
+cv2.destroyWindow("Video")
+writer.release()
+
